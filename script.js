e76cb419a876e22e89db1312e1035d804dc5e13f
Dan Gorelick <dqgorelick@gmail.com>
2019-07-18 11:08:29 -0400

    WIP

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const randRange = (min, max) => {
   return Math.floor(Math.random()*(max-min)) + min;
 }
@@ -7,9 +6,9 @@ const map = (n, start1, stop1, start2, stop2) => {
   return ((n-start1)/(stop1-start1))*(stop2-start2)+start2;
 };
 
-const ANIMATION_TIME = 2.5
+const ANIMATION_TIME = 1.5
 
-// code from: https://medium.com/@francoisromain/smooth-a-svg-path-with-cubic-bezier-curves-e37b49d46c74
+// SVG bezier path code from: https://medium.com/@francoisromain/smooth-a-svg-path-with-cubic-bezier-curves-e37b49d46c74
 
 const svgPath = (points, command) => {
   // build the d attributes by looping over the points
@@ -33,21 +32,11 @@ const line = (pointA, pointB) => {
 
 const lineCommand = point => `L ${point[0]} ${point[1]}`
 
-// Position of a control point
-// I:  - current (array) [x, y]: current point coordinates
-//     - previous (array) [x, y]: previous point coordinates
-//     - next (array) [x, y]: next point coordinates
-//     - reverse (boolean, optional): sets the direction
-// O:  - (array) [x,y]: a tuple of coordinates
-
 const controlPoint = (current, previous, next, reverse) => {
-  // When 'current' is the first or last point of the array
-  // 'previous' or 'next' don't exist.
-  // Replace with 'current'
   const p = previous || current
   const n = next || current
   // The smoothing ratio
-  const smoothing = 1.1
+  const smoothing = 0
   // Properties of the opposed-line
   const o = line(p, n)
   // If is end-control-point, add PI to the angle to go backward
@@ -59,11 +48,6 @@ const controlPoint = (current, previous, next, reverse) => {
   return [x, y]
 }
 
-// Create the bezier curve command
-// I:  - point (array) [x,y]: current point coordinates
-//     - i (integer): index of 'point' in the array 'a'
-//     - a (array): complete array of points coordinates
-// O:  - (string) 'C x2,y2 x1,y1 x,y': SVG cubic bezier C command
 const bezierCommand = (point, i, a) => {
   // start control point
   const [cpsX, cpsY] = controlPoint(a[i - 1], a[i - 2], point)
@@ -72,22 +56,22 @@ const bezierCommand = (point, i, a) => {
   return `C ${cpsX},${cpsY} ${cpeX},${cpeY} ${point[0]},${point[1]}`
 }
 
-
 const generatePoints = (num, offsetX) => {
-  const scaleX = 15
-  const scaleY = (window.innerHeight / (num) ) * 0.8;
+  const scaleX = window.innerHeight / 88
+  const scaleY = (window.innerHeight / (num));
   // const scale
   const points = []
   // dir = 1;
   for (let i=0; i<num; i++) {
-    let dir = Math.random() < 0.5 ? -1 : 1;
-    points.push([offsetX + dir * 0.20 * (i) * randRange(50, 150),  500 - i*scaleY])
-    // points.push([offsetX ,  500 - i*scaleY])
+    const dir = Math.random() < 0.5 ? -1 : 1;
+    const x = offsetX + dir * 0.20 * (i) * randRange(0, 300)
+    const y = window.innerHeight - i * scaleY
+    // const x = offsetX
+    points.push([x, y])
   }
+  points.push([offsetX, -10])
   return points
 }
-
-// const points = [[5, 10], [10, 40], [40, 30], [60, 5], [90, 45], [120, 10], [150, 45], [200, 10]]
 
 const svg = document.querySelector('.svg')
 
@@ -99,9 +83,6 @@ const createSVGPath = (key, path, animation, id) => {
 
     const color = keyColors[key]
     const len = newPath.getTotalLength()
-    // newPath.setAttributeNS(null, 'style', `--stroke: ${color}; --len: ${len}`);
-
-    // offset is always the length * 2
     const path_offset = len
     const start = len
     const initial = len
@@ -115,8 +96,6 @@ const createSVGPath = (key, path, animation, id) => {
       --end: ${end};
       --initial: ${initial};
       --segment_length: ${segment_length}`)
-
-    // newPath.setAttributeNS(null, 'style', `stroke: red`);
 
     return newPath
 }
@@ -137,33 +116,20 @@ const getMidiID = (midi) => {
 }
 
 const startNote = (key, id) => {
-  // const points = generatePoints(10, window.innerWidth/2)
-  const steps = window.innerWidth/38
-  const points = generatePoints(10,  (10 + (key % 12)) * steps)
+  playingNotes = true;
+  backgroundHaze(true);
+  const steps = window.innerWidth/40
+  const points = generatePoints(7,  ((key) - 40) * steps)
   const d = svgPath(points, bezierCommand)
   const path = createSVGPath(key, d, 'animating', id);
-  // const pathEnd = createSVGPath(key, d, '', id + '_end');
   svg.appendChild(path);
-  // svg.appendChild(pathEnd);
 }
 
 const endNote = (midi) => {
   const id = getMidiID(midi)
   const path = document.getElementById(id)
-  // const pathEnd = document.getElementById(id + '_end')
   const matrix = getComputedStyle(path).getPropertyValue('stroke-dasharray')
   const dashArrayStart = parseFloat(matrix.split('px')[0], 10)
-
-
-
-  // // offset is always the length * 2
-  // const path_offset = len
-  // const start = len
-  // const initial = len
-  // const segment_length = len
-  // const end = len*2
-
-
 
   const len = path.getTotalLength()
   const color = keyColors[midi]
@@ -182,20 +148,14 @@ const endNote = (midi) => {
     --start_midway: ${(start + end)/2};
     --segment_length: ${segment_length}`)
   path.setAttributeNS(null, 'class', 'animatingEnd')
+  playingNotes = false
   setTimeout(() => {
+    if (!playingNotes) {
+      backgroundHaze(false);
+    }
     svg.removeChild(path)
-    // svg.removeChild(pathEnd)
-  }, 1000 * 3) // TODO: MAKE THIS DYNAMIC
+  }, 1000 * ANIMATION_TIME)
 }
-
-const upper = [81,50,87,51,69,82,53,84,54,89,55,85,73,57,79,48,80,219,187,221]
-const lower = [90,83,88,68,67,86,71,66,72,78,74,77,188,76,190,186,191]
-const keysPressed = {}
-
-
-const vol = new Tone.Volume(12)
-const synth = duoSynth()
-Tone.bufferSize = 2048*2;
 
 function duoSynth() {
     return new Tone.PolySynth(5, Tone.DuoSynth, {
@@ -217,22 +177,68 @@ function initMidi() {
         // Listen for a 'note on' message on all channels
         input.addListener('noteon', "all",
           function (e) {
-            synth.triggerAttackRelease(Tone.Frequency(e.note.number, "midi").eval());
-            activateNote(e.note.number)
+            const midi = e.note.number
+            if (!keysPressed[midi]) {
+              keysPressed[midi] = true
+              // console.log(midi)
+              const id = addKeyCount(midi)
+              synth.triggerAttackRelease(Tone.Frequency(midi, "midi").eval())
+              startNote(midi, midi + '_' + id)
+            }
           }
         );
 
         // Listen to pitch bend message on channel 3
         input.addListener('noteoff', "all",
           function (e) {
+            const midi = e.note.number
             synth.triggerRelease(Tone.Frequency(e.note.number, "midi").eval());
-            disableNote(e.note.number)
+            keysPressed[midi] = false
+            synth.triggerRelease(Tone.Frequency(midi, "midi").eval())
+            endNote(midi)
           }
         );
 
       })
   });
 }
+
+const keyColors = [];
+
+const setKeyColors = () => {
+    var min = 0;
+    var max = 88;
+    var startingHue = randRange(0, 256)
+
+    for (var i=min; i<max+1; i++) {
+        keyColors.push(map(i, min, max, 0, 1000))%256
+        // keyColors.push(randRange(0, 256))
+    }
+}
+
+
+const backgroundHaze = (on) => {
+    // if (on) {
+    //     console.log('turning on!')
+    //     backgroundHue = (backgroundHue + 4)%256
+    //     $('body').css('background-color', 'hsla('+ backgroundHue +', 100%, 40%, 0.2)')
+    // } else {
+    //     console.log('turning off!')
+    //     $('body').css('background-color', '#222222')
+    // }
+}
+
+var playingNotes = false;
+var backgroundHue = randRange(0, 256)
+
+const upper = [81,50,87,51,69,82,53,84,54,89,55,85,73,57,79,48,80,219,187,221]
+const lower = [90,83,88,68,67,86,71,66,72,78,74,77,188,76,190,186,191]
+const keysPressed = {}
+
+
+const vol = new Tone.Volume(12)
+const synth = duoSynth()
+Tone.bufferSize = 2048*2;
 
 const init = () => {
   document.addEventListener('keydown', function(e) {
@@ -242,7 +248,7 @@ const init = () => {
       if (up !== -1 && !keysPressed[key]) {
           const midi = up + 60
           keysPressed[key] = true
-          // console.log(midi)
+          console.log(midi)
           const id = addKeyCount(midi)
           synth.triggerAttackRelease(Tone.Frequency(midi, "midi").eval())
           startNote(midi, midi + '_' + id)
@@ -250,7 +256,7 @@ const init = () => {
       if (low !== -1 && !keysPressed[key]) {
           const midi = low + 48
           keysPressed[key] = true
-          // console.log(midi)
+          console.log(midi)
           const id = addKeyCount(midi)
           synth.triggerAttackRelease(Tone.Frequency(midi, "midi").eval())
           startNote(midi, midi + '_' + id)
@@ -274,9 +280,7 @@ const init = () => {
           endNote(midi)
       }
   });
-
   initMidi();
-
 }
 
 
@@ -285,17 +289,3 @@ StartAudioContext(Tone.context, '.starter-button').then(function(){
     setKeyColors();
     init();
 });
-
-
-
-const keyColors = [];
-
-const setKeyColors = () => {
-    var min = 0;
-    var max = 88;
-    var startingHue = randRange(0, 256)
-
-    for (var i=min; i<max+1; i++) {
-        keyColors.push(map(i, min, max, 0, 400))%256
-    }
-}
